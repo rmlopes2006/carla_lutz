364103c5f5a72e76a6cbd3cbe9478caa3205ce8a
Rodrigo Martins <rmlopes2006@hotmail.com>
2019-03-10 21:27:18 -0300

    Iniciando a desenho.html.

--- web/js/main.js
+++ web/js/main.js
@@ -1,3 +1,4 @@
+/* Slide */
 $(".rslides").responsiveSlides({
   auto: true,             // Boolean: Animate automatically, true or false
   speed: 800,             // Integer: Speed of the transition, in milliseconds
@@ -17,6 +18,7 @@ $(".rslides").responsiveSlides({
   after: function(){}     // Function: After callback
 });
 
+/* Animações */
 Visibility.onVisible(function(){
 	/*Anima menu Smartphone */
 	setTimeout(function(){
@@ -98,3 +100,35 @@ Visibility.onVisible(function(){
 		$(".moldura3").addClass("animated fadeInDown");
 	}, 800);
 });
+
+/* Slideshow galerias */
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
